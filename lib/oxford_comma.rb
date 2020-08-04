93429d9d184e4c70fb55bdd0d1d19fcb7ae1831e
Joshua Tall <tall.joshua1984@yahoo.com>
2020-08-04 02:57:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-    if array.count <= 1
-    array.join
-  elsif array.count == 2
-    array.join(" and ")
-  else
-    last = array.pop
-    array.join(", ") + ", and " << last
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.insert(1, "and").join(" ")
+  elsif array.size == 3
+    array.last.insert(-10, "and ")
+    array.join(", ")
+  else array.size > 3
+    array.join(", ") << ", and #{array[-1]}"
   end
 end
